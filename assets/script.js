8077d0d3092db7f25287b2501731640981d6f04f
lbako801 <lbako98@gmail.com>
2022-11-27 19:05:49 -0700

    redid ids and classes. Changed color scheme and CSS code.

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+
+
+const questions = [
+  {
+    question: 'What is an Array??',
+    answers: [
+      { text: 'A collection of items stored at contiguous memory locations.', correct: true },
+      { text: 'A linear data structure in which the elements are not necessarily stored in a contiguous manner.', correct: false }
+    ]
+  },
+  {
+    question: 'What is a common phrase coded when first learning a language?',
+    answers: [
+      { text: 'Hello, World!', correct: true },
+      { text: 'What is up?', correct: false },
+      { text: 'Testing, testing, 1, 2, 3', correct: false },
+      { text: 'Howdy', correct: false }
+    ]
+  },
+  {
+    question: 'What tag can be used to insert a line break or blank line in an HTML document?',
+    answers: [
+      { text: '<body></body>', correct: false },
+      { text: '<br></br>', correct: true },
+      { text: '<head></head>', correct: false },
+      { text: '<title></title>', correct: false }
+    ]
+  },
+  {
+    question: 'What is the name of CSS design that calls for fluid and adaptable elements based on the device resolution or size?',
+    answers: [
+      { text: 'Shifting', correct: false },
+      { text: 'Responsive', correct: true }
+    ]
+  }
+]
