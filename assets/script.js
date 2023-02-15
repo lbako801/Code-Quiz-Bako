bda3e826594bbeccc9a49af4f023f749d1d21ffe
lbako801 <lbako98@gmail.com>
2023-02-14 22:19:12 -0700

    fully functioning code quiz!

--- assets/script.js
+++ assets/script.js
@@ -53,7 +53,63 @@ var questionChoices = [
             { text: "Katherine Johnson", correct: false },
             { text: "Maryam Mirzakhani", correct: false },
         ]
-    }
+    },
+    {
+        question: "Approximately, how many coding languages are in existence?",
+        answers: [
+            { text: "~30", correct: false },
+            { text: "~500", correct: false },
+            { text: "~700", correct: true },
+            { text: "~1500", correct: false },
+        ]
+    },
+    {
+        question: "What was the direct predecessor to C?",
+        answers: [
+            { text: "Assembly", correct: false },
+            { text: "Objective-C", correct: false },
+            { text: "Basic", correct: false },
+            { text: "B", correct: true },
+        ]
+    },
+    {
+        question: "Angular was made by which company?",
+        answers: [
+            { text: "Microsoft", correct: false },
+            { text: "Apple", correct: false },
+            { text: "Google", correct: true },
+            { text: "Facebook", correct: false },
+        ]
+    },
+    {
+        question: "Tim Berners-Lee wrote this product in 1990. What is it called?",
+        answers: [
+            { text: "HAL/S", correct: false },
+            { text: "HTML", correct: true },
+            { text: "HLSL", correct: false },
+            { text: "HAXE", correct: false },
+        ]
+    },
+    {
+        question: "This language was named after a gem. What is it?",
+        answers: [
+            { text: "Agate", correct: false },
+            { text: "Aquamarine", correct: false },
+            { text: "Ruby", correct: true },
+            { text: "Amethyst", correct: false },
+        ]
+    },
+    {
+        question: "Visual Basic, a third generation language, was released in 1991 and declared legacy in 2008. Which corporation released it?",
+        answers: [
+            { text: "Intel", correct: false },
+            { text: "General Magic", correct: false },
+            { text: "Microsoft", correct: true },
+            { text: "NEXT", correct: false },
+        ]
+    },
+    
+    
 ];
 
 //HMTL Elements below
@@ -64,13 +120,19 @@ var answerContainer = document.getElementById('answer-buttons')
 var answerChoices = document.getElementById('answer-choice')
 var nextBtn = document.getElementById('next-btn')
 var timer = document.getElementById('time-num')
-startBtn.addEventListener('click', startQuiz)
-nextBtn.addEventListener('click', nextQuestion)
+
 //Basic variables and values for quiz beginning
 let currentQuestionPosition;
 var time = 45;
 var timerCount;
 
+//Event listeners for quiz navigation
+startBtn.addEventListener('click', startQuiz)
+nextBtn.addEventListener('click', () => {
+    currentQuestionPosition++
+    console.log(currentQuestionPosition);
+    nextQuestion()
+});
 
 //Below function starts the quiz on the even listener being triggered
 function startQuiz() {
@@ -78,18 +140,22 @@ function startQuiz() {
     startBtn.classList.add('hide');
     //Displays the question container on start button click
     questionContainer.classList.remove('hide');
+    //Initiates timer countdown
     timerCount = setInterval(countDown, 1000);
+    //Sets the HTML time content to current time which is 45 seconds at the start
     timer.textContent = time;
-    grabQuestion()
+      //shuffles the question array
+    shuffledQuestions = questionChoices.sort(() => Math.random() - .5);
+    //Sets index of current question to 0 to keep track of quiz completion
+    currentQuestionPosition = 0
+    //Runs the grabQuestion function to get a question and question array
+    grabQuestion(shuffledQuestions)
 };
 
 //Below function grabs the question and its answers
-function grabQuestion() {
-    //shuffles the question array
-    shuffledQuestion = questionChoices.sort(() => Math.random() - .5);
-    //Sets the shuffled question as index 0.
-    currentQuestionPosition = 0
-    currentQuestion = shuffledQuestion[currentQuestionPosition]
+function grabQuestion(shuffledQuestions) {
+
+    currentQuestion = shuffledQuestions[currentQuestionPosition];
     //adds the question text into the question ID in HTML
     questionText.innerHTML = currentQuestion.question;
 
@@ -104,7 +170,6 @@ function grabQuestion() {
         answerContainer.appendChild(answerBtn);
         answerBtn.addEventListener('click', answerCheck);
     });
-    
 
 };
 
@@ -114,17 +179,19 @@ function answerCheck(event) {
     const choice = event.target;
     //Grabs the value of the clicked answer
     const choiceValue = choice.dataset.correct;
-  
+    
+    
     //Checks if clicked choice is equal to 'true'. Alters time based on comparison.
     if (choiceValue === 'true') {
-        time += 5
+        time += 1
     } else {
-        time -= 15
+        time -= 10
     };
 };
 
 //Below function resets the question container and adds a new question
 function resetContainer() {
+    //removes all children of the answer button parent container
     while (answerContainer.hasChildNodes()) {
        answerContainer.removeChild(answerContainer.firstChild)
    }
@@ -132,14 +199,34 @@ function resetContainer() {
 
 //After container is reset, we use the below function to add a new question
 function nextQuestion() {
-
+    //resets container and deletes last question info
     resetContainer()
-    grabQuestion(shuffledQuestion[currentQuestionPosition])
-}
+    //if there are more questions, send them to player. Otherwise end quiz.
+    if (shuffledQuestions.length > currentQuestionPosition) {
+        grabQuestion(shuffledQuestions)
+    } else {
+        window.localStorage.setItem(time)
+        quizEnd()
+    }
+    
+};
 
-//This function starts and updates the timer
+function quizEnd() {
+    window.location.replace = "./end.html";
+    highScore = document.getElementById('scores');
+    window.localStorage.getItem(time)
+    highScoreLi = document.createElement('li');
+    highScoreLi.innerText = time;
+
+};
+
+
+//This function starts and updates the timer. Ends quiz if time reaches 0.
 function countDown() {
     time--;
     timer.textContent = time;
-    // if users is out of time, show GAME OVER
+    if (time < 0) {
+        quizEnd()
+    } 
 };
+
